802736873c7d06ec92ccb1760018eaa4487bc0ed
Venkata sai <84610958+venkatasai12345@users.noreply.github.com>
2023-01-28 16:35:28 +0530

    Create Binary Tree Zigzag Level Order Traversal.java

---
+++ 27-1-23/Binary Tree Zigzag Level Order Traversal.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> list=new LinkedList<>();
+         
+        if(root==null){
+            
+            return list;
+        }
+        Queue<TreeNode> man=new LinkedList<>();
+        man.add(root);
+        boolean gun=true;
+        while(!man.isEmpty()){
+            int size=man.size();
+            LinkedList<Integer> fun=new LinkedList<>();
+            for(int i=0;i<size;i++){
+                TreeNode temp=man.poll();
+                if(gun){
+                    fun.add(temp.val);
+                }else{
+                    fun.addFirst(temp.val);
+                }
+                if(temp.left!=null){
+                    man.add(temp.left);
+                }if(temp.right!=null){
+                    man.add(temp.right);
+                }
+            }
+            list.add(fun);
+            gun=!gun;
+        }
+     return list;
+
+    }
+}
