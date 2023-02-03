011e379fd36752dab34c7bca0b4af096a740ff80
Venkata sai <84610958+venkatasai12345@users.noreply.github.com>
2023-02-03 15:36:26 +0530

    Update Balanced Binary Tree.java

--- 3-02-23/Balanced Binary Tree.java
+++ 3-02-23/Balanced Binary Tree.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    boolean result=true;
+    public int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int m=height(root.left);
+        int n=height(root.right);
+        int k=Math.abs(m-n);
+        if(k!=0 && k!=1){
+            result=false;
+        }
+        return Math.max(m,n)+1;
+    }
+    public boolean isBalanced(TreeNode root) {
+        height(root);
+        return result;
+    }
+}
